bbcc22eb0fd1d07553ecf321b5b82b75ece70bcd
Nadine Seibel <nadine.seibel@bi-so.de>
2023-08-09 14:58:14 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { registerPlugin } from '@capacitor/core';
+
+import type { MLKitBarcodeScannerPlugin } from './definitions';
+
+const MLKitBarcodeScanner = registerPlugin<MLKitBarcodeScannerPlugin>(
+  'MLKitBarcodeScanner',
+  {
+    web: () => import('./web').then(m => new m.MLKitBarcodeScannerWeb()),
+  },
+);
+
+export * from './definitions';
+export { MLKitBarcodeScanner };
---
+++ src/web.ts
@@ -0,0 +1,13 @@
+import { WebPlugin } from '@capacitor/core';
+
+import type { MLKitBarcodeScannerPlugin } from './definitions';
+
+export class MLKitBarcodeScannerWeb
+  extends WebPlugin
+  implements MLKitBarcodeScannerPlugin
+{
+  async echo(options: { value: string }): Promise<{ value: string }> {
+    console.log('ECHO', options);
+    return options;
+  }
+}

6880dac5b026bbe712e8c700245de801e75f5b4c
Nadine Seibel <nadine.seibel@bi-so.de>
2024-03-26 13:55:55 +0100

    fixed eslint & prettier issues

--- src/web.ts
+++ src/web.ts
@@ -6,6 +6,8 @@ export class MlKitBarcodeScannerWeb
   extends WebPlugin
   implements MlKitBarcodeScannerPlugin
 {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async scan(_settings: ISettings): Promise<IResult> {
     return Promise.reject(new Error("PLATFORM_NOT_SUPPORTED"));
   }

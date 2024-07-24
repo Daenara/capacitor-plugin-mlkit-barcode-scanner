88e4612527ccd563b880dad40e8c5e4170916f21
Nadine Seibel <nadine.seibel@bi-so.de>
2024-07-24 13:08:57 +0200

    seems I didn't actually build after adding comments to shut the linter up

--- dist/esm/web.js
+++ dist/esm/web.js
@@ -1,5 +1,6 @@
 import { WebPlugin } from '@capacitor/core';
 export class MlKitBarcodeScannerWeb extends WebPlugin {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async scan(_settings) {
         return Promise.reject(new Error("PLATFORM_NOT_SUPPORTED"));
     }
